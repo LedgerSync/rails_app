93c6dfd09ba4e4fe980b377c4fe3769bca49b7b2
Ryan Jackson <ryanwjackson@gmail.com>
2019-07-14 21:35:42 -0400

    Update router

--- app/lib/util/router.rb
+++ app/lib/util/router.rb
@@ -4,21 +4,18 @@ module Util
   class Router # :nodoc:
     include Rails.application.routes.url_helpers
 
-    attr_reader :test
-
-    def initialize(test: false)
-      @test = test
-
-      # Remove URL routes from router as they will not work in capybara.
-      return unless test
-
-      self.class.instance_methods.map(&:to_s).select { |e| e.ends_with?('_url') }.each do |method|
-        self.instance_eval("undef #{method}")
-      end
-    end
-
     def login_url
       Settings.application.login_url
+    end
+
+    if Rails.env.test?
+      instance_methods.map(&:to_s).each do |method|
+        next unless method.ends_with?('_url')
+
+        define_method(method) do
+          raise "Method #{method} not available in test.  Please use *_path instead."
+        end
+      end
     end
   end
 end
