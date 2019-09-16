93e77af309bcb383c5a03846a992a18558bb7596
Ryan Jackson <ryanwjackson@gmail.com>
2019-09-16 12:01:34 +0200

    Fix tests: https://travis-ci.org/LedgerSync/rails_app/builds/585497635

--- spec/api/search/v1/search_spec.rb
+++ spec/api/search/v1/search_spec.rb
@@ -16,8 +16,8 @@ describe 'GET api/search/v1/ledgers/:id/:lib_resource_type', type: :api do
       search_api_get "#{path}?query=foobarbaz"
       expect_object_of_type('lib_search_result')
       expect(json.dig(:data, :attributes, :query)).to eq('foobarbaz')
-      expect(json.dig(:data, :attributes, :next_page_url)).to eq('http://example.org/api/search/v1/ledgers/ldgr_2/customers?pagination%5Bcursor%5D=asdf&query=foobarbaz')
-      expect(json.dig(:data, :attributes, :previous_page_url)).to eq('http://example.org/api/search/v1/ledgers/ldgr_2/customers?pagination%5Bcursor%5D=asdf&query=foobarbaz')
+      expect(json.dig(:data, :attributes, :next_page_url)).to eq("http://example.org/api/search/v1/ledgers/#{ledger.id}/customers?pagination%5Bcursor%5D=asdf&query=foobarbaz")
+      expect(json.dig(:data, :attributes, :previous_page_url)).to eq("http://example.org/api/search/v1/ledgers/#{ledger.id}/customers?pagination%5Bcursor%5D=asdf&query=foobarbaz")
     end
   end
 end
