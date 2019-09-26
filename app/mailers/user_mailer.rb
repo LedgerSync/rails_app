30ac282aa8f9e3e36ad02b07ea03d8bf1a6b8997
Ryan Jackson <ryanwjackson@gmail.com>
2019-09-26 19:48:08 -0400

    Override mail method when using the UserMailer to better enforce disabling

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,23 +1,14 @@
 # frozen_string_literal: true
 
 class UserMailer < ApplicationMailer
-  after_action { do_not_deliver unless deliver? }
-
   private
 
-  def deliver?
-    return false if Settings.mailer.disable_email_to_users
-
-    defined?(@deliver) ? @deliver : true
-  end
-
-  def do_deliver
-    mail.perform_deliveries = true
-    @deliver = true
-  end
-
-  def do_not_deliver
-    mail.perform_deliveries = false
-    @deliver = false
+  def mail(user:, **keywords)
+    mail.perform_deliveries = false if Settings.mailer.disable_email_to_users
+    super(
+      **keywords.merge(
+        to: user.email
+      )
+    )
   end
 end
